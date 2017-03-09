4f5c4f764a24fa62155b3cb25787bc8410665107
Clase <Clase>
2017-03-08 19:28:43 -0500

    Creación de Proyecto

---
+++ BankingSystem/ClassLibrary1/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Account
+    {
+
+        private String _name;
+        public String getName(){
+        return _name;
+        }
+        public void setName(String name){
+
+        _name= name;
+          }
+
+     }
+    
+}
+
